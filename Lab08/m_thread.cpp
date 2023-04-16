c210de3221aefe662c386a787fde30ae45fab9c7
RafayAkbar <94800480+RafayAkbar@users.noreply.github.com>
2023-04-17 02:03:15 +0500

    Update m_thread.cpp

--- Lab08/m_thread.cpp
+++ Lab08/m_thread.cpp
@@ -1,92 +1,97 @@
-#include <iostream>
 #include <pthread.h>
 #include <time.h>
-#define MAX 30
-#define THREAD_MAX 6
-
-using namespace std;
-
+#include <stdlib.h>
+#define MAX 15
+#define THREAD_MAX 4
 int a[MAX];
 int part = 0;
 
-void merge(int low, int mid, int high){
-int* left = new int[mid - low + 1];
-int* right = new int[high - mid];
-int n1 = mid - low + 1, n2 = high - mid, i, j;
+void merge(int low, int mid, int high)
+{   
+    int* left = (int*) malloc( (mid - low + 1) * sizeof(int));
+    int* right = (int*) malloc( (high - mid) * sizeof(int));
 
-for (i = 0; i < n1; i++)
-left[i] = a[i + low];
+    int n1 = mid - low + 1,
+    n2 = high - mid,
+    i, j;
 
-for (i = 0; i < n2; i++)
-right[i] = a[i + mid + 1];
+    for (i = 0; i < n1; i++)
+        left[i] = a[i + low];
 
-int k = low;
-i = j = 0;
+    for (i = 0; i < n2; i++)
+        right[i] = a[i + mid + 1];
 
-while (i < n1 && j < n2) {
-if (left[i] <= right[j])
-a[k++] = left[i++];
-else
-a[k++] = right[j++];
+    int k = low;
+    i = j = 0;
+
+    while (i < n1 && j < n2) {
+        if (left[i] <= right[j])
+            a[k++] = left[i++];
+        else
+            a[k++] = right[j++];
+    }
+
+    while (i < n1) {
+        a[k++] = left[i++];
+    }
+    while (j < n2) {
+        a[k++] = right[j++];
+    }
+
+    free(left);
+    free(right);
 }
 
-while (i < n1) {
-a[k++] = left[i++];
-}
-while (j < n2) {
-a[k++] = right[j++];
-}
-}
+void merge_sort(int low, int high)
+{
 
-void merge_sort(int low, int high){
-int mid = low + (high - low) / 2;
-if (low < high) {
-
-merge_sort(low, mid);
-
-merge_sort(mid + 1, high);
-
-merge(low, mid, high);
-}
+    int mid = low + (high - low) / 2;
+    if (low < high) {
+        merge_sort(low, mid);
+        merge_sort(mid + 1, high);
+        merge(low, mid, high);
+    }
 }
 
-void* merge_sort(void* arg){
+void* merge_sortthread(void* arg)
+{
+    int thread_part = part++:
+    int low = thread_part * (MAX / THREAD_MAX);
+    int high = (thread_part + 1) * (MAX / THREAD_MAX) - 1;
 
-int thread_part = part++;
-
-int low = thread_part * (MAX / 6);
-int high = (thread_part + 1) * (MAX / 6) - 1;
-
-int mid = low + (high - low) / 2;
-if (low < high) {
-merge_sort(low, mid);
-merge_sort(mid + 1, high);
-merge(low, mid, high);
-
-}
-return 0;
+    int mid = low + (high - low) / 2;
+    if (low < high) {
+        merge_sort(low, mid);
+        merge_sort(mid + 1, high);
+        merge(low, mid, high);
+    }
+    return 0;
 }
 
-int main(){
-for (int i = 0; i < MAX; i++)
-a[i] = rand() % 100;
+int main()
+{
+    for (int i = 0; i < MAX; i++){
+        a[i] = rand() % 100;
+    }
 
-pthread_t threads[THREAD_MAX];
+    pthread_t threads[THREAD_MAX];
 
-for (int i = 0; i < THREAD_MAX; i++)
-pthread_create(&threads[i], NULL, merge_sort,
-(void*)NULL);
+    for (int i = 0; i < THREAD_MAX; i++)
+        pthread_create(&threads[i], NULL, merge_sort123,
+                       (void*)NULL);
 
-for (int i = 0; i < 6; i++)
-pthread_join(threads[i], NULL);
+    for (int i = 0; i < THREAD_MAX; i++)
+        pthread_join(threads[i], NULL);
 
-merge(0, (MAX / 2 - 1) / 2, MAX / 2 - 1);
-merge(MAX / 2, MAX/2 + (MAX-1-MAX/2)/2, MAX - 1);
-merge(0, (MAX - 1)/2, MAX - 1);
+    merge(0, (MAX / 2 - 1) / 2, MAX / 2 - 1);
+    merge(MAX / 2, MAX/2 + (MAX-1-MAX/2)/2, MAX - 1);
+    merge(0, (MAX - 1)/2, MAX - 1);
 
-cout << "\nSorted array is: ";
-for (int i = 0; i < MAX; i++)
-cout << a[i] << " ";
+    printf("\n\nSorted array: ");
+    for (int i = 0; i < MAX; i++)
+        printf ("%d ", a[i]);
 
-return 0;
+
+    printf("\n");
+    return 0;
 }
