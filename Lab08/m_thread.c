3fd4154e9ae2ab1356f3a1cf7fca0ba5c3c43a8e
RafayAkbar <94800480+RafayAkbar@users.noreply.github.com>
2023-05-03 10:31:05 +0500

    Update m_thread.c

--- Lab08/m_thread.c
+++ Lab08/m_thread.c
@@ -1,123 +1,109 @@
+// Importing required libraries
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
 
+// Setting maximum size of the array
 #define MAX_SIZE 100000
 
-int arr[MAX_SIZE];
-int n, thread_count;
+// Global variables
+int arr[MAX_SIZE]; // array to be sorted
+int n, thread_count; // n: size of the array, thread_count: number of threads
 
+// Struct to represent range of array
 typedef struct {
-    int low;
-    int high;
+int low;
+int high;
 } range;
 
+// Function to merge two sorted subarrays
 void merge(int low, int mid, int high) {
-    int* left_arr = (int*) malloc((mid - low + 1) * sizeof(int));
-    int* right_arr = (int*) malloc((high - mid) * sizeof(int));
+// Dynamically allocating memory for temporary arrays
+int* left_arr = (int*) malloc((mid - low + 1) * sizeof(int));
+int* right_arr = (int*) malloc((high - mid) * sizeof(int));
 
-    int i, j, k;
+  int i, j, k;
 
-    for (i = 0; i < mid - low + 1; i++) {
-        left_arr[i] = arr[low + i];
-    }
+// Copying values to temporary arrays
+for (i = 0; i < mid - low + 1; i++) {
+    left_arr[i] = arr[low + i];
+}
 
-    for (j = 0; j < high - mid; j++) {
-        right_arr[j] = arr[mid + 1 + j];
-    }
+for (j = 0; j < high - mid; j++) {
+    right_arr[j] = arr[mid + 1 + j];
+}
 
-    i = 0;
-    j = 0;
-    k = low;
+// Merging two subarrays
+i = 0;
+j = 0;
+k = low;
 
-    while (i < mid - low + 1 && j < high - mid) {
-        if (left_arr[i] <= right_arr[j]) {
-            arr[k] = left_arr[i];
-            i++;
-        } else {
-            arr[k] = right_arr[j];
-            j++;
-        }
-        k++;
-    }
-
-    while (i < mid - low + 1) {
+while (i < mid - low + 1 && j < high - mid) {
+    if (left_arr[i] <= right_arr[j]) {
         arr[k] = left_arr[i];
         i++;
-        k++;
-    }
-
-    while (j < high - mid) {
+    } else {
         arr[k] = right_arr[j];
         j++;
-        k++;
     }
-
-    free(left_arr);
-    free(right_arr);
+    k++;
 }
 
+// Copying remaining elements to merged array
+while (i < mid - low + 1) {
+    arr[k] = left_arr[i];
+    i++;
+    k++;
+}
+
+while (j < high - mid) {
+    arr[k] = right_arr[j];
+    j++;
+    k++;
+}
+
+// Freeing dynamically allocated memory
+free(left_arr);
+free(right_arr);
+// Function to sort subarrays recursively
 void* merge_sort(void* r) {
-    range* p = (range*) r;
+// Converting void pointer to range pointer
+range* p = (range*) r;
+    // Getting low and high indexes of subarray
+int low = p->low;
+int high = p->high;
 
-    int low = p->low;
-    int high = p->high;
+// Checking if subarray has more than 1 element
+if (low < high) {
+    int mid = (low + high) / 2;
 
-    if (low < high) {
-        int mid = (low + high) / 2;
+    // Checking if more than 1 thread can be used
+    if (thread_count > 1) {
+        thread_count -= 2;
 
-        if (thread_count > 1) {
-            thread_count -= 2;
+        // Creating two threads for two subarrays
+        pthread_t left_tid;
+        pthread_t right_tid;
 
-            pthread_t left_tid;
-            pthread_t right_tid;
+        range left_range = { low, mid };
+        range right_range = { mid + 1, high };
 
-            range left_range = { low, mid };
-            range right_range = { mid + 1, high };
+        pthread_create(&left_tid, NULL, merge_sort, &left_range);
+        pthread_create(&right_tid, NULL, merge_sort, &right_range);
 
-            pthread_create(&left_tid, NULL, merge_sort, &left_range);
-            pthread_create(&right_tid, NULL, merge_sort, &right_range);
+        // Waiting for threads to finish
+        pthread_join(left_tid, NULL);
+        pthread_join(right_tid, NULL);
 
-            pthread_join(left_tid, NULL);
-            pthread_join(right_tid, NULL);
-
-            thread_count += 2;
-        } else {
-            merge_sort(&((range){ low, mid }));
-            merge_sort(&((range){ mid + 1, high }));
-        }
-
-        merge(low, mid, high);
+        thread_count += 2;
+    } else {
+        // Sorting subarrays recursively
+        merge_sort(&((range){ low, mid }));
+        merge_sort(&((range){ mid + 1, high }));
     }
 
-    return NULL;
+    // Merging two sorted subarrays
+    merge(low, mid, high);
 }
 
-int main() {
-    printf("Enter the size of the array: ");
-    scanf("%d", &n);
-
-    printf("Enter the elements of the array: ");
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
-    }
-
-    printf("Enter the number of threads to be used: ");
-    scanf("%d", &thread_count);
-
-    pthread_t tid;
-
-    range r = { 0, n - 1 };
-    pthread_create(&tid, NULL, merge_sort, &r);
-
-    pthread_join(tid, NULL);
-
-    printf("Sorted array: ");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-
-    return 0;
-}
-
+return NULL;
